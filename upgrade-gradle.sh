cb90c40917684a9c97c811b1e306d21b976a1a4f
EdwarDDay <4127904+EdwarDDay@users.noreply.github.com>
2021-04-10 12:12:45 +0200

    Fallback release channel, if no version is present (#9)

--- upgrade-gradle.sh
+++ upgrade-gradle.sh
@@ -2,21 +2,49 @@
 
 set -e
 
-distributionType=$1
+distributionType="$1"
 # verify distribution type
-if [ "$distributionType" != "bin" ] && [ "$distributionType" != "all" ] && [ "$distributionType" != "default" ]; then
+if [ "$distributionType" != 'bin' ] && [ "$distributionType" != 'all' ] && [ "$distributionType" != 'default' ]; then
   echo "::error::Invalid distribution type \"$distributionType\" - must be \"bin\", \"all\" or \"default\""
   exit 1
 fi
 
-releaseChannel=$2
-if [ "$releaseChannel" != "current" ] && [ "$releaseChannel" != "release-candidate" ] &&
-  [ "$releaseChannel" != "nightly" ] && [ "$releaseChannel" != "release-nightly" ]; then
+releaseChannel="$2"
+# verify release channel
+if [ "$releaseChannel" != 'current' ] && [ "$releaseChannel" != 'release-candidate' ] &&
+  [ "$releaseChannel" != 'nightly' ] && [ "$releaseChannel" != 'release-nightly' ]; then
   echo "::error::Invalid release channel \"$releaseChannel\" - must be one of \"current\", \"release-candidate\", \"nightly\" or \"release-nightly\""
   exit 2
 fi
 
-latestVersion=$(curl "https://services.gradle.org/versions/$releaseChannel" | jq --raw-output '.version')
+echo "::debug::calling: https://services.gradle.org/versions/$releaseChannel"
+versionInfo=$(curl "https://services.gradle.org/versions/$releaseChannel")
+latestVersion=$(echo "$versionInfo" | jq --raw-output '.version // ""')
+
+while [ -z "$latestVersion" ]; do
+  case "$releaseChannel" in
+  'current')
+    echo '::error::No version in release channel current'
+    echo "::error:: Version information from gradle: $versionInfo"
+    exit 3
+    ;;
+  'release-candidate')
+    echo '::warn::No version in channel release-candidate, switch to current'
+    releaseChannel='current'
+    ;;
+  'release-nightly')
+    echo '::warn::No version in channel release-nightly, switch to nightly'
+    releaseChannel='nightly'
+    ;;
+  'nightly')
+    echo '::warn::No version in channel nightly, switch to release-candidate'
+    releaseChannel='release-candidate'
+    ;;
+  esac
+  echo "::debug::calling: https://services.gradle.org/versions/$releaseChannel"
+  versionInfo=$(curl "https://services.gradle.org/versions/$releaseChannel")
+  latestVersion=$(echo "$versionInfo" | jq --raw-output '.version')
+done
 
 echo "::debug::Latest gradle version: $latestVersion"
 echo "::set-output name=gradle-version::$latestVersion"
