a1722347d90dc19596d09c58c15ace01bf3d9c66
EdwarDDay <4127904+EdwarDDay@users.noreply.github.com>
2021-04-11 11:57:19 +0200

    Add version information to action output (#14)

--- upgrade-gradle.sh
+++ upgrade-gradle.sh
@@ -17,13 +17,11 @@ if [ "$releaseChannel" != 'current' ] && [ "$releaseChannel" != 'release-candida
   exit 2
 fi
 
-versionInfo=''
 latestVersion=''
 
 function retrieveLatestVersion() {
   echo "::debug::calling: https://services.gradle.org/versions/$releaseChannel"
-  versionInfo=$(curl "https://services.gradle.org/versions/$releaseChannel")
-  latestVersion=$(echo "$versionInfo" | jq --raw-output '.version // ""')
+  latestVersion=$(curl "https://services.gradle.org/versions/$releaseChannel" | jq --raw-output '.version // ""')
 }
 
 retrieveLatestVersion
@@ -32,7 +30,6 @@ while [ -z "$latestVersion" ]; do
   case "$releaseChannel" in
   'current')
     echo '::error::No version in release channel current'
-    echo "::error:: Version information from gradle: $versionInfo"
     exit 3
     ;;
   'release-candidate')
@@ -63,3 +60,56 @@ fi
 
 # update gradle wrapper
 ./gradlew wrapper
+
+# https://github.community/t/set-output-truncates-multiline-strings/16852/3
+function escapeVariable() {
+  local content="$1"
+  local content="${content//'%'/'%25'}"
+  local content="${content//$'\n'/'%0A'}"
+  local content="${content//$'\r'/'%0D'}"
+  echo "$content"
+}
+
+function retrieveInformation() {
+  local path="$1"
+  # assume no information on service fail - probably 404
+  (curl --fail "https://services.gradle.org/$path/$latestVersion" || echo '[]') | jq --raw-output '.[] | ("- [" + .key + "](" + .link + ") " + .summary )'
+}
+
+# add information to release notes page
+versionInformation="Upgrade to latest [gradle version $latestVersion](https://docs.gradle.org/$latestVersion/release-notes.html)"
+
+# add information to fixed issues
+echo "::debug::calling https://services.gradle.org/fixed-issues/$latestVersion"
+fixedIssues=$(retrieveInformation 'fixed-issues')
+
+if [ -n "$fixedIssues" ]; then
+  versionInformation="$versionInformation
+
+<details><summary>fixed issues</summary>
+
+$fixedIssues
+</details>"
+fi
+
+fixedIssues=$(escapeVariable "$fixedIssues")
+echo "::set-output name=fixed-issues::$fixedIssues"
+
+# add information to known issues
+echo "::debug::calling https://services.gradle.org/known-issues/$latestVersion"
+knownIssues=$(retrieveInformation 'known-issues')
+
+if [ -n "$knownIssues" ]; then
+  versionInformation="$versionInformation
+
+<details><summary>known issues</summary>
+
+$knownIssues
+</details>"
+fi
+
+knownIssues=$(escapeVariable "$knownIssues")
+echo "::set-output name=known-issues::$knownIssues"
+
+versionInformation=$(escapeVariable "$versionInformation")
+echo "::set-output name=version-information::$versionInformation"
