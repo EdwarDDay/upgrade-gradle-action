3a0479af3fcf0829bb81c189d6320ebe1f22301f
EdwarDDay <4127904+EdwarDDay@users.noreply.github.com>
2024-11-06 17:17:02 +0100

    Fix test root directory error (#72)

--- .github/scripts/test-upgrade-gradle.sh
+++ .github/scripts/test-upgrade-gradle.sh
@@ -6,6 +6,6 @@
 
 set -e
 
-gradle init --type basic --dsl kotlin --project-name test-project
+gradle init --overwrite --type basic --dsl kotlin --project-name test-project
 
 ./gradlew wrapper --gradle-version 6.0
